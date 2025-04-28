bb18325c5d7fedc483681539cf63b7716c1266b7
UE3B52 <u11102252@o365.ttu.edu.tw>
2025-04-29 02:07:57 +0800

    Add files via upload
    ###Third Commit
    ##完成Part3，使用for、while與if迴圈完成不斷輸入直至符合範圍，根據輸入字元排序成直角三角形，清除螢幕返回主選單

--- P2E3B52.c
+++ P2E3B52.c
@@ -48,8 +48,48 @@ int main(void) {
     printf("| b. 顯示乘法表      |\n");
     printf("| c. 結束            |\n");
     printf("----------------------");
+    char in;
+    printf("\n請輸入一個字元:");
+    scanf(" %c",&in);
+    if(in=='a'||in=='A'){
+        system("cls");
+    }
+    char inn;
+    int r;
+    while (1) {
+        printf("\n請輸入一個 a~n 的字元: ");
+        scanf(" %c", &inn);        
+
+        if (inn >= 'a' && inn <= 'n') {         
+            for (r = 0; r<= inn - 'a'; r++) {
+                char start = inn - r;
+                for (char ch = start; ch <= inn; ch++)
+                    printf("%c", ch);
+                printf("\n");
+            }
+            break;                               
+        } 
+        else {                               
+            printf("\n輸入範圍錯誤，請重新輸入\n");
+        }
+    }
+    printf("\n按任意鍵返回主選單...\n");
+    system("pause");
+    getchar( );        
+    system("cls");
+    printf("----------------------\n");
+    printf("| a. 畫出直角三角形  |\n");
+    printf("| b. 顯示乘法表      |\n");
+    printf("| c. 結束            |\n");
+    printf("----------------------\n");
+
+
+
+
+    
     system("pause");
     return 0;
+
 
 
 
